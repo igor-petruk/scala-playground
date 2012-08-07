7945f6df834a36e813ca88f25b7fe5bdd821bc04
Igor Petruk <igor.petrouk@gmail.com>
2012-08-08 02:04:24 +0300

    Added cake

--- src/main/scala/playground/Main.scala
+++ src/main/scala/playground/Main.scala
@@ -1,9 +1,44 @@
 package playground
 
 object Main {
+  case class Apple(size:Int)
+  
+  trait AppleEaterComponentApi{
+    def eater: AppleEater
 
+    trait AppleEater{
+      def eat
+    }
+  }
+  
+  trait AppleProducerComponentApi{
+    def producer: AppleProducer
+    
+    trait AppleProducer{
+      def produce:Apple
+    }
+  }
+  
+  trait WormComponent extends AppleEaterComponentApi{
+    self: AppleProducerComponentApi =>
+    
+    lazy val eater = new AppleEater{
+      def eat{
+        val apple = producer.produce
+        println("Eating "+apple)
+      }
+    }
+  }
+
+  trait AppleTreeComponent extends AppleProducerComponentApi{
+    lazy val producer = new AppleProducer {
+      def produce = Apple(10)
+    }
+  }  
+  
   def main(args: Array[String]){
-    println("Welcome Scala")
+    val core = new AppleTreeComponent with WormComponent
+    core.eater.eat
   }
 
 }
