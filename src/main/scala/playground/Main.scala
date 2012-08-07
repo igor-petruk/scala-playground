c952f5d8725ba78aed52058881615dd7622696d2
Igor Petruk <igor.petrouk@gmail.com>
2012-08-08 02:17:04 +0300

    Added "using"

--- src/main/scala/playground/Main.scala
+++ src/main/scala/playground/Main.scala
@@ -1,9 +1,21 @@
 package playground
 
+import java.io.{FileOutputStream, PrintWriter}
+
 object Main {
 
+  def using[T <: {def close():Unit},R](value:T)(f: T=>R):R={
+    try{
+      f(value)
+    }finally{
+      value.close
+    }  
+  }
+  
   def main(args: Array[String]){
-    println("Welcome Scala")
+    using(new PrintWriter(new FileOutputStream("test"))){file=>
+      file.println("Hello")
+    }
   }
 
 }
