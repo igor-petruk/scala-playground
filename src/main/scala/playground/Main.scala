90d7dda26fe4d4dada0612e4f73ceb66be00bb05
Igor Petruk <igor.petrouk@gmail.com>
2012-08-08 01:52:42 +0300

    Added traits and stackable aop

--- src/main/scala/playground/Main.scala
+++ src/main/scala/playground/Main.scala
@@ -1,9 +1,38 @@
 package playground
 
+import playground.Main.NumberHolder
+
 object Main {
 
+  trait OddEven{
+    def odd:Boolean
+    def even = !odd
+  }
+
+  trait Value{
+    def value:Int
+  }
+
+  trait ByTwo extends Value{
+    def even: Boolean
+    def byTwo = if (even) value/2 else sys.error("Oops")
+  }
+  
+  class NumberHolder(fValue:Int) extends OddEven with ByTwo{
+    def odd = value % 2 != 0
+    def value = fValue
+  }
+
+  trait LoggedValue extends Value{
+    abstract override def value:Int={
+      println("Logged %d".format(super.value))
+      super.value
+    }
+  }
+
   def main(args: Array[String]){
-    println("Welcome Scala")
+    val nh = new NumberHolder(10) with LoggedValue
+    println(nh.byTwo)
   }
 
 }
